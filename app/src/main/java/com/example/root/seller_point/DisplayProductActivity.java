ee09168830c9145fc54b2dc0f49f7c39671c2a95
vyanktesh-sutaria <vsutaria72@gmail.com>
2017-11-08 13:09:57 +0530

    test branch commit

--- app/src/main/java/com/example/root/seller_point/DisplayProductActivity.java
+++ app/src/main/java/com/example/root/seller_point/DisplayProductActivity.java
@@ -1,20 +1,96 @@
 package com.example.root.seller_point;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.io.ByteArrayOutputStream;
 
 public class DisplayProductActivity extends AppCompatActivity {
 
     FloatingActionButton fabAddProd;
+    TextView tv;
+    ImageView img;
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_product);
 
         fabAddProd = findViewById(R.id.fabAddProduct);
+        Button b =findViewById(R.id.button);
+
+        tv = (TextView)findViewById(R.id.textView2);
+        img = (ImageView)findViewById(R.id.imageView2);
+
+        b.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                Intent pickIntent = new Intent(Intent.ACTION_PICK);
+                pickIntent.setType("image/*");
+                //pickIntent.setAction();
+
+                Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                String pickTitle = "Select or take a new Picture"; // Or get from strings.xml
+                Intent chooserIntent = Intent.createChooser(pickIntent, pickTitle);
+                chooserIntent.putExtra
+                        (
+                                Intent.EXTRA_INITIAL_INTENTS,
+                                new Intent[]{takePhotoIntent}
+                        );
+
+                startActivityForResult(chooserIntent, 123);
+            }
+        });
+    }
+
+    public void onActivityResult(int requestCode,int resultCode,Intent data)
+    {
+        if(data == null)
+        {}
+        else if(data.getData()==null && resultCode == Activity.RESULT_OK)
+        {
+            Bitmap photo = (Bitmap)data.getExtras().get("data");
+
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            photo.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+
+            tv.setText(photo.toString());//Base64.encodeToString(outputStream.toByteArray(), Base64.DEFAULT));
+            img.setImageBitmap(photo);
+        }
+        else if(data.getData()!=null && resultCode == Activity.RESULT_OK) {
+            Uri selectedImage = data.getData();
+            Bitmap map = null;
+            try {
+                map = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImage);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            map.compress(Bitmap.CompressFormat.JPEG, 50, outputStream);
+
+            tv.setText(map.toString());//Base64.encodeToString(outputStream.toByteArray(), Base64.DEFAULT));
+            img.setImageBitmap(map);
+        }
     }
 
     public void fabAddProd_click(View v){
--- app/src/main/java/com/example/root/seller_point/ProductActivity.java
+++ app/src/main/java/com/example/root/seller_point/ProductActivity.java
@@ -1,5 +1,6 @@
 package com.example.root.seller_point;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,6 +8,13 @@ import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class ProductActivity extends AppCompatActivity {
 
@@ -25,6 +33,8 @@ public class ProductActivity extends AppCompatActivity {
         spincategory = findViewById(R.id.spincategory);
         spincategory.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,str));
         spinsubcategory = findViewById(R.id.spinsubcategory);
+
+        new RestaurantRegistraion().execute();
     }
 
     public void Chkfreeship_click(View v){
@@ -35,6 +45,115 @@ public class ProductActivity extends AppCompatActivity {
         else{
             txtshipcharge.setVisibility(View.INVISIBLE);
             txtshipcharge.setHint("");
+        }
+    }
+
+    public void doWork(String args)
+    {
+        Toast.makeText(ProductActivity.this,"This is post execute",Toast.LENGTH_SHORT).show();
+    }
+
+    public  class RestaurantRegistraion extends AsyncTask<String,Void,String>
+    {
+//        @Override
+//        protected void onPreExecute() {
+//            pd = new ProgressDialog(MainActivity.this);
+//            pd.setTitle("Loading");
+//            pd.setMessage("Please Wait...");
+//            pd.setCancelable(false);
+//            pd.show();
+//            super.onPreExecute();
+//        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            String response = "";
+            String link = "http://192.168.0.104/sellerapi/public/index.php/api/tblCategory";
+            try {
+                URL url = new URL(link);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("GET");
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.setDoOutput(false);
+                httpURLConnection.connect();
+
+//                result = getStringImage(bitmap);
+//                Uri.Builder builder = new Uri.Builder()
+//                        .appendQueryParameter("RestaurantName",params[0])
+//                        .appendQueryParameter("MobileNumber",params[1])
+//                        .appendQueryParameter("LandlineNumber",params[2])
+//                        .appendQueryParameter("Email",params[3])
+//                        .appendQueryParameter("Address",params[4])
+//                        .appendQueryParameter("RestaurantType",params[5])
+//                        .appendQueryParameter("Description",params[6])
+//                        .appendQueryParameter("RestaurantImage",result)
+//                        .appendQueryParameter("OpeningTime",params[7])
+//                        .appendQueryParameter("ClosingTime",params[8])
+//                        .appendQueryParameter("Username",params[9])
+//                        .appendQueryParameter("Password",params[10])
+//                        .appendQueryParameter("StarsID",params[11])
+//                        .appendQueryParameter("GSTNumber",params[12])
+//                        .appendQueryParameter("IsGSTIncluded",params[13]);
+//
+//                String qry = builder.build().getEncodedQuery();
+
+//                OutputStream os = httpURLConnection.getOutputStream();
+//                OutputStreamWriter osw = new OutputStreamWriter(os);
+//                BufferedWriter bw = new BufferedWriter(osw);
+//                bw.write(qry);
+//                bw.flush();
+//                bw.close();
+//                os.close();
+
+                int responsecode = httpURLConnection.getResponseCode();
+                if(responsecode ==  HttpURLConnection.HTTP_OK)
+                {
+                    InputStream is = httpURLConnection.getInputStream();
+                    InputStreamReader isr = new InputStreamReader(is);
+                    BufferedReader br = new BufferedReader(isr);
+                    String line = "";
+
+                    while((line = br.readLine())!=null)
+                    {
+                        response = response + line;
+                    }
+
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+//            pd.dismiss();
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+//            pd.dismiss();
+
+            Toast.makeText(ProductActivity.this,result,Toast.LENGTH_SHORT).show();
+//            try {
+//                JSONObject jsonObject = new JSONObject(result);
+//                int code = jsonObject.getInt("code");
+//                String message = jsonObject.getString("message");
+//                if(code == 100)
+//                {
+//                    Toast.makeText(getApplicationContext(),message,Toast.LENGTH_LONG).show();
+//                }
+//                else if(code == 101)
+//                {
+//                    Toast.makeText(getApplicationContext(),message,Toast.LENGTH_LONG).show();
+//                }
+//                else if(code == 102)
+//                {
+//                    Toast.makeText(getApplicationContext(),message,Toast.LENGTH_LONG).show();
+//                }
+//
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+
+            super.onPostExecute(result);
         }
     }
 }
